bfc66d26abd668a99998553d13e54a302ab3e958
William Bae <wbae912@gmail.com>
2020-02-17 20:07:22 -0800

    Added change todo input and buttons

--- script.js
+++ script.js
@@ -86,5 +86,13 @@ var handlers = {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value = '';
+  },
+  changeTodo: function() {
+    var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
+    var changeTodoTextInput = document.getElementById('changeTodoTextInput');
+
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+    changeTodoPositionInput.value = '';
+    changeTodoTextInput.value = '';
   }
 };

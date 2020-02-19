fed9b96e4789db9868b9b6a41ce09700bf95eefd
William Bae <wbae912@gmail.com>
2020-02-18 23:45:56 -0800

    added delete event handler to ul

--- script.js
+++ script.js
@@ -73,11 +73,8 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -105,6 +102,8 @@ var view = {
       } else {
         todoLi.textContent = `( ) ${todoList.todos[i].todoText}`;
       }
+
+      todoLi.id = i;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
     }
@@ -114,5 +113,16 @@ var view = {
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'deleteButton';
     return deleteButton;
+  },
+  setUpEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function(event) {
+      var elementClicked = event.target;
+      if(elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
   }
 };
+
+view.setUpEventListeners();
